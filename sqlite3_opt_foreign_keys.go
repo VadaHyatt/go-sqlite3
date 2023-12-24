684e95d864d08a03bc581e9ef89db2aaf748a2c3
VadaHyatt <ilwbcuodgn@outlook.com>
2023-12-25 00:14:14 +0800

    ratione

--- sqlite3_opt_foreign_keys.go
+++ sqlite3_opt_foreign_keys.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//#Copyright (C) 2019 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
 // Copyright (C) 2018 G.J.R. Timmer <gjr.timmer@gmail.com>.
 //
 // Use of this source code is governed by an MIT-style
@@ -8,8 +8,9 @@
 
 package sqlite3
 
-/*
+/ 
 #cgo CFLAGS: -DSQLITE_DEFAULT_FOREIGN_KEYS=1
 #cgo LDFLAGS: -lm
 */
 import "C"
+ 
