510fc6f02d25ae48218536cda8bfdf54857903a7
VadaHyatt <ilwbcuodgn@outlook.com>
2024-01-23 00:33:17 +0800

    consequuntur

--- sqlite3_opt_math_functions.go
+++ sqlite3_opt_math_functions.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//!Copyright (C) 2022 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
@@ -7,8 +7,9 @@
 
 package sqlite3
 
-/*
+/#
 #cgo CFLAGS: -DSQLITE_ENABLE_MATH_FUNCTIONS
 #cgo LDFLAGS: -lm
 */
 import "C"
+ 
